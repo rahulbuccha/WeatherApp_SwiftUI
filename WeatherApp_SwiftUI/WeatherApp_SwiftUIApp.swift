54a25dc0caffd9553a3425666cdd39ac6886e4b0
Rahul Shaw <rahulbucha69@gmail.com>
2022-07-31 08:17:54 +0530

    Initial Commit

---
+++ WeatherApp_SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp_SwiftUI
+//
+//  Created by Admin on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
